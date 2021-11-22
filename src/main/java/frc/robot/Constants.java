271301967f6fa6448e8434b81e635aa0c364fb66
csokolove <94080789+csokolove@users.noreply.github.com>
2021-11-22 07:47:44 -0500

    Update branch compatibility

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,6 +3,9 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
+
+import org.frc5587.lib.pid.FPID;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -13,6 +16,40 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+    public static final class DrivetrainConstants {
+        // motor ports 
+        public static final int LEFT_LEADER = 10;
+        public static final int LEFT_FOLLOWER = 11;
+        public static final int RIGHT_LEADER = 15;
+        public static final int RIGHT_FOLLOWER = 16;
+
+        public static final boolean LEFT_SIDE_INVERTED = true;
+        public static final boolean RIGHT_SIDE_INVERTED = true;
+        
+        // PID constants
+        public static final FPID TURN_FPID = new FPID(0, 0.1, 0, 0.009);
+        public static final double TURN_PID_TOLERANCE_DEG = 0.5;
+        public static final double TURN_PID_FORWARD_THROTTLE = 0;
+        public static final double TURN_PID_UPDATE_PERIOD_SEC = 0.010;
+        public static final boolean INVERT_GYRO_DIRECTION = true;
+
+        // motor current limit constants
+        public static final int SMART_CURRENT_LIMIT = 37;
+        public static final int HARD_CURRENT_LIMIT = 45;
+        public static final double SMART_CURRENT_LIMIT_DELAY = 0.2; // seconds
+        public static final StatorCurrentLimitConfiguration STATOR_CURRENT_LIMIT_CONFIGURATION = new StatorCurrentLimitConfiguration(true, DrivetrainConstants.SMART_CURRENT_LIMIT, DrivetrainConstants.HARD_CURRENT_LIMIT, DrivetrainConstants.SMART_CURRENT_LIMIT_DELAY);
+    
+        public static final int TICKS_PER_REV = 8192;
+        public static final double WHEEL_DIAMETER_METERS = 0.1524;
+        public static final double WHEEL_RADIUS_METERS = WHEEL_DIAMETER_METERS / 2;
+        public static final int HISTORY_LIMIT = 32;
+    }
+
+    public static final class LEDConstants {
+        public static final int PWM_PORT = 0;
+        public static final int LED_LENGTH = 300; 
+    }
+    
     public static final class IntakeConstants {
         // motor ports
         public static final int RIGHT_MOTOR = 20;
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,36 +5,57 @@
 package frc.robot;
 
 
+import org.frc5587.lib.control.DeadbandJoystick;
 import org.frc5587.lib.control.DeadbandXboxController;
+import org.frc5587.lib.advanced.AddressableLEDController;
 
+import frc.robot.commands.ArcadeDrive;
+import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Intake;
+
+import frc.robot.Constants.LEDConstants;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls). Instead, the structure of the robot (including subsystems,
+ * commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // subsystems
-  private final Intake intake = new Intake();
-  // controllers
-  private final DeadbandXboxController xb = new DeadbandXboxController(1);
-  // commands
+    // Controllers
+    private final DeadbandJoystick joy = new DeadbandJoystick(0, 1.5);
+    private final DeadbandXboxController xb = new DeadbandXboxController(1);
+    // Subsystems
+    private final Drivetrain drivetrain = new Drivetrain();
+    private final Intake intake = new Intake();
+    // Commands
+    private final ArcadeDrive arcadeDrive = new ArcadeDrive(drivetrain, joy::getY, () -> -joy.getXCurveDampened());
+    // Others
+    private final AddressableLEDController ledController = new AddressableLEDController(LEDConstants.PWM_PORT,
+            LEDConstants.LED_LENGTH);
   
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+     // make drivetrain use arcadeDrive to drive
+    drivetrain.setDefaultCommand(arcadeDrive);
     // Configure the button bindings
     configureButtonBindings();
+    
+    ledController.startLEDStepHandlerNotifier((Integer step, AddressableLEDBuffer buffer) -> {
+      return ledController.stretchRainbow(50 * 10, step, buffer);
+    }, 0.02);
   }
 
   /**
