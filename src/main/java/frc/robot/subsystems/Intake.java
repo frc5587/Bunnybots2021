93db85bca23e3d05866fdfbbea517a2a0ccab3c0
csokolove <csokolove6@gmail.com>
2021-11-27 17:22:54 -0500

    Fixed SpeedControllerGroup

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,6 +4,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 
@@ -11,6 +12,8 @@ import frc.robot.Constants.IntakeConstants;
 public class Intake extends SubsystemBase {
     private final CANSparkMax rightIntake = new CANSparkMax(IntakeConstants.RIGHT_MOTOR, MotorType.kBrushless);
     private final CANSparkMax leftIntake = new CANSparkMax(IntakeConstants.LEFT_MOTOR, MotorType.kBrushless);
+
+    private final SpeedControllerGroup intakeMotors = new SpeedControllerGroup(rightIntake, leftIntake);
 
     public Intake() {
         super();
@@ -36,23 +39,20 @@ public class Intake extends SubsystemBase {
      * Moves intake forwards
      */
     public void forward() {
-        rightIntake.set(IntakeConstants.THROTTLE);
-        leftIntake.set(IntakeConstants.THROTTLE);
+        intakeMotors.set(IntakeConstants.THROTTLE);
     }
 
     /**
      * Moves intake backwards
      */
     public void backward() {
-        rightIntake.set(-IntakeConstants.THROTTLE);
-        leftIntake.set(-IntakeConstants.THROTTLE);
+        intakeMotors.set(-IntakeConstants.THROTTLE);
     }
 
     /**
      * Stops intake motors
      */
     public void stop() {
-        rightIntake.set(0);
-        leftIntake.set(0);
+        intakeMotors.set(0);
     }
 }
