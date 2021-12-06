2c896cdc6fb028182684ceaa31e1d2aeb67083e8
Ben B <lerminator3@gmail.com>
2021-12-05 23:32:15 -0500

    reverted and fixed NullPointerException handling

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -24,42 +24,25 @@ public class Arm extends PivotingArmBase {
     }
 
     @Override
-    public double getEncoderValue(EncoderValueType valueType) {
-        switch(valueType) {
-            case Position:
-            return this.leader.getSelectedSensorPosition();
-            case Velocity:
-            return this.leader.getSelectedSensorVelocity();
-            default:
-            return 0;
-        }
-    }
-
-    @Override
-    public void setEncoderPosition(double position) {
-        this.leader.setSelectedSensorPosition(position);
-    }
-
-    @Override
     public void configureMotors() {
         try {
             this.leader.configFactoryDefault();
+            this.leader.setNeutralMode(NeutralMode.Brake);
+            this.leader.setInverted(ArmConstants.LEADER_INVERTED);
+            resetEncoders();
         }
         catch(NullPointerException e) {
-            System.out.println("NullPointerException " + e + " from arm leader configFactoryDefault");
+            System.out.println("NullPointerException " + e + " from arm leader motor");
         }
-        this.leader.setNeutralMode(NeutralMode.Brake);
-        this.leader.setInverted(ArmConstants.LEADER_INVERTED);
-
-        for(WPI_TalonFX follower : this.followers) {
-            try {
+        try {
+            for(WPI_TalonFX follower : this.followers) {
                 follower.configFactoryDefault();
+                follower.setNeutralMode(NeutralMode.Brake);
+                follower.setInverted(ArmConstants.FOLLOWERS_INVERTED);
             }
-            catch(NullPointerException e) {
-                System.out.println("NullPointerException " + e + " from arm follower configFactoryDefault");
-            }
-            follower.setNeutralMode(NeutralMode.Brake);
-            follower.setInverted(ArmConstants.FOLLOWERS_INVERTED);
+        }
+        catch(NullPointerException e) {
+            System.out.println("NullPointerException " + e + " from arm follower motor");
         }
     }
 }
