5da6432652a207a2e80b8c13edb5a7629683bacf
Andrew Lawlor <lawlorcube06@gmail.com>
2022-01-11 16:58:42 -0500

    reformatted code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -44,160 +44,160 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-    // Controllers
-    private final DeadbandJoystick joy = new DeadbandJoystick(0, 1.5);
-    private final DeadbandXboxController xboxController = new DeadbandXboxController(1);
-    // Subsystems
-    private final Drivetrain drivetrain = new Drivetrain();
-    private final BunnyDumper bunnyDumper = new BunnyDumper();
-    private final Intake intake = new Intake();
-    private final Arm arm = new Arm();
-    // Commands
-    private final ArcadeDrive arcadeDrive = new ArcadeDrive(drivetrain, joy::getY, () -> -joy.getXCurveDampened());
-    private final IntakeCrate intakeCrate = new IntakeCrate(intake);
-    private final EjectCrate ejectCrate = new EjectCrate(intake);
-    private final ArmUp armUp = new ArmUp(arm);
-    private final ArmDown armDown = new ArmDown(arm);
-    private final ArmUp autoArmUp = new ArmUp(arm);
-    private final ArmDown autoArmDown = new ArmDown(arm);
-    // Auto paths
-    private final RamseteCommandWrapper getRightBox = new RamseteCommandWrapper(drivetrain,
-            new AutoPath("get right box"), AutoConstants.RAMSETE_CONSTANTS);
-    private final RamseteCommandWrapper dropOffRightBox = new RamseteCommandWrapper(drivetrain,
-            new AutoPath("drop off right box"), AutoConstants.RAMSETE_CONSTANTS);
-    private final RamseteCommandWrapper backupAndCenter = new RamseteCommandWrapper(drivetrain,
-            new AutoPath("backup and center"), AutoConstants.RAMSETE_CONSTANTS);
-    // Currently unused auto paths, but may be used in the future
-    private final RamseteCommandWrapper getRightBox2 = new RamseteCommandWrapper(drivetrain,
-            new AutoPath("get right box 2"), AutoConstants.RAMSETE_CONSTANTS);
-    private final RamseteCommandWrapper dropOffRightBox2 = new RamseteCommandWrapper(drivetrain,
-            new AutoPath("drop off right box 2"), AutoConstants.RAMSETE_CONSTANTS);
-    private final RamseteCommandWrapper backupAndCenter2 = new RamseteCommandWrapper(drivetrain,
-            new AutoPath("backup and center 2"), AutoConstants.RAMSETE_CONSTANTS);
-    // Testing auto paths
-    private final RamseteCommandWrapper sPath = new RamseteCommandWrapper(drivetrain, new AutoPath("s path"),
-            AutoConstants.RAMSETE_CONSTANTS).setOdometryToFirstPoseOnStart();
-    private final RamseteCommandWrapper y2Meters = new RamseteCommandWrapper(drivetrain,
-            new Pose2d(0, 0, new Rotation2d(0)), new ArrayList<Translation2d>(),
-            new Pose2d(0, 2, new Rotation2d(0)),
-            AutoConstants.RAMSETE_CONSTANTS);
-    private final RamseteCommandWrapper x2Meters = new RamseteCommandWrapper(drivetrain,
-            new Pose2d(0, 0, new Rotation2d(0)), new ArrayList<Translation2d>(),
-            new Pose2d(2, 0, new Rotation2d(0)),
-            AutoConstants.RAMSETE_CONSTANTS);
-
-    /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
-     */
-    public RobotContainer() {
-        // make drivetrain use arcadeDrive to drive
-        drivetrain.setDefaultCommand(arcadeDrive);
-        // Configure the button bindings
-        configureButtonBindings();
-    }
-
-    /**
-     * Use this method to define your button->command mappings. Buttons can be
-     * created by
-     * instantiating a {@link GenericHID} or one of its subclasses ({@link
-     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
-     * it to a {@link
-     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-     */
-    private void configureButtonBindings() {
-        // Y Button for Intake controls
-        JoystickButton yButton = new JoystickButton(xboxController, XboxController.Button.kY.value);
-        // B Button for Bunny Dumper controls
-        JoystickButton bButton = new JoystickButton(xboxController, XboxController.Button.kB.value);
-        // Left Trigger for Intake & Bunny Dumper controls
-        Trigger leftTrigger = new Trigger(() -> xboxController.getTrigger(Hand.kLeft));
-        // DPad Up for higher arm setpoint
-        POVButton dpadUp = new POVButton(xboxController, 0);
-        // DPad Down for lower arm setpoint
-        POVButton dpadDown = new POVButton(xboxController, 180);
-        // arm limit switch
-        Trigger armLimitSwitch = new Trigger(() -> arm.getLimitSwitchValue());
-
-        // Bunny Dumper
-        /*
-         * when b button and left trigger are pressed together, extend the pistons. when
-         * the two buttons are released, retract the pistons
-         */
-        bButton.and(leftTrigger).whenActive(bunnyDumper::extend, bunnyDumper)
-                .whenInactive(bunnyDumper::retract, bunnyDumper);
-
-        // Intake
-        /**
-         * when y button is active, move intake inwards.
-         * when the y button is inactive, stop.
-         */
-        yButton.and(leftTrigger.negate()).whenActive(intake::forward, intake).whenInactive(intake::stop,
-                intake);
+        // Controllers
+        private final DeadbandJoystick joy = new DeadbandJoystick(0, 1.5);
+        private final DeadbandXboxController xboxController = new DeadbandXboxController(1);
+        // Subsystems
+        private final Drivetrain drivetrain = new Drivetrain();
+        private final BunnyDumper bunnyDumper = new BunnyDumper();
+        private final Intake intake = new Intake();
+        private final Arm arm = new Arm();
+        // Commands
+        private final ArcadeDrive arcadeDrive = new ArcadeDrive(drivetrain, joy::getY, () -> -joy.getXCurveDampened());
+        private final IntakeCrate intakeCrate = new IntakeCrate(intake);
+        private final EjectCrate ejectCrate = new EjectCrate(intake);
+        private final ArmUp armUp = new ArmUp(arm);
+        private final ArmDown armDown = new ArmDown(arm);
+        private final ArmUp autoArmUp = new ArmUp(arm);
+        private final ArmDown autoArmDown = new ArmDown(arm);
+        // Auto paths
+        private final RamseteCommandWrapper getRightBox = new RamseteCommandWrapper(drivetrain,
+                        new AutoPath("get right box"), AutoConstants.RAMSETE_CONSTANTS);
+        private final RamseteCommandWrapper dropOffRightBox = new RamseteCommandWrapper(drivetrain,
+                        new AutoPath("drop off right box"), AutoConstants.RAMSETE_CONSTANTS);
+        private final RamseteCommandWrapper backupAndCenter = new RamseteCommandWrapper(drivetrain,
+                        new AutoPath("backup and center"), AutoConstants.RAMSETE_CONSTANTS);
+        // Currently unused auto paths, but may be used in the future
+        private final RamseteCommandWrapper getRightBox2 = new RamseteCommandWrapper(drivetrain,
+                        new AutoPath("get right box 2"), AutoConstants.RAMSETE_CONSTANTS);
+        private final RamseteCommandWrapper dropOffRightBox2 = new RamseteCommandWrapper(drivetrain,
+                        new AutoPath("drop off right box 2"), AutoConstants.RAMSETE_CONSTANTS);
+        private final RamseteCommandWrapper backupAndCenter2 = new RamseteCommandWrapper(drivetrain,
+                        new AutoPath("backup and center 2"), AutoConstants.RAMSETE_CONSTANTS);
+        // Testing auto paths
+        private final RamseteCommandWrapper sPath = new RamseteCommandWrapper(drivetrain, new AutoPath("s path"),
+                        AutoConstants.RAMSETE_CONSTANTS).setOdometryToFirstPoseOnStart();
+        private final RamseteCommandWrapper y2Meters = new RamseteCommandWrapper(drivetrain,
+                        new Pose2d(0, 0, new Rotation2d(0)), new ArrayList<Translation2d>(),
+                        new Pose2d(0, 2, new Rotation2d(0)),
+                        AutoConstants.RAMSETE_CONSTANTS);
+        private final RamseteCommandWrapper x2Meters = new RamseteCommandWrapper(drivetrain,
+                        new Pose2d(0, 0, new Rotation2d(0)), new ArrayList<Translation2d>(),
+                        new Pose2d(2, 0, new Rotation2d(0)),
+                        AutoConstants.RAMSETE_CONSTANTS);
 
         /**
-         * when y button & left trigger are active, move intake outwards.
-         * when the y button & left trigger are inactive, stop.
+         * The container for the robot. Contains subsystems, OI devices, and commands.
          */
-        yButton.and(leftTrigger).whenActive(intake::backward, intake).whenInactive(intake::stop, intake);
-
-        // Arm
-        /**
-         * when the limit switch is pressed down, reset the encoders
-         */
-        armLimitSwitch.whenActive(arm::resetEncoders);
+        public RobotContainer() {
+                // make drivetrain use arcadeDrive to drive
+                drivetrain.setDefaultCommand(arcadeDrive);
+                // Configure the button bindings
+                configureButtonBindings();
+        }
 
         /**
-         * when the up button on the d-pad is pressed, move the arm up to a predefined
-         * setpoint
+         * Use this method to define your button->command mappings. Buttons can be
+         * created by
+         * instantiating a {@link GenericHID} or one of its subclasses ({@link
+         * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+         * it to a {@link
+         * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
          */
-        dpadUp.whenPressed(armUp);
+        private void configureButtonBindings() {
+                // Y Button for Intake controls
+                JoystickButton yButton = new JoystickButton(xboxController, XboxController.Button.kY.value);
+                // B Button for Bunny Dumper controls
+                JoystickButton bButton = new JoystickButton(xboxController, XboxController.Button.kB.value);
+                // Left Trigger for Intake & Bunny Dumper controls
+                Trigger leftTrigger = new Trigger(() -> xboxController.getTrigger(Hand.kLeft));
+                // DPad Up for higher arm setpoint
+                POVButton dpadUp = new POVButton(xboxController, 0);
+                // DPad Down for lower arm setpoint
+                POVButton dpadDown = new POVButton(xboxController, 180);
+                // arm limit switch
+                Trigger armLimitSwitch = new Trigger(() -> arm.getLimitSwitchValue());
+
+                // Bunny Dumper
+                /**
+                 * when b button and left trigger are pressed together, extend the pistons. when
+                 * the two buttons are released, retract the pistons
+                 */
+                bButton.and(leftTrigger).whenActive(bunnyDumper::extend, bunnyDumper)
+                                .whenInactive(bunnyDumper::retract, bunnyDumper);
+
+                // Intake
+                /**
+                 * when y button is active, move intake inwards.
+                 * when the y button is inactive, stop.
+                 */
+                yButton.and(leftTrigger.negate()).whenActive(intake::forward, intake).whenInactive(intake::stop,
+                                intake);
+
+                /**
+                 * when y button & left trigger are active, move intake outwards.
+                 * when the y button & left trigger are inactive, stop.
+                 */
+                yButton.and(leftTrigger).whenActive(intake::backward, intake).whenInactive(intake::stop, intake);
+
+                // Arm
+                /**
+                 * when the limit switch is pressed down, reset the encoders
+                 */
+                armLimitSwitch.whenActive(arm::resetEncoders);
+
+                /**
+                 * when the up button on the d-pad is pressed, move the arm up to a predefined
+                 * setpoint
+                 */
+                dpadUp.whenPressed(armUp);
+
+                /**
+                 * when the down button on the d-pad is pressed, move the arm down to a
+                 * predefined setpoint
+                 */
+                dpadDown.whenPressed(armDown);
+        }
 
         /**
-         * when the down button on the d-pad is pressed, move the arm down to a
-         * predefined setpoint
+         * Use this to pass the autonomous command to the main {@link Robot} class.
+         *
+         * @return the command to run in autonomous
          */
-        dpadDown.whenPressed(armDown);
-    }
+        public Command getAutonomousCommand() {
+                // * Simple tests
+                // Use these to verify odometry, gains, and encoder stuff. For the x2Meters, lay
+                // out a measuring tape to verify its moving 2 meters, and for the getRightBox,
+                // set it up on the field and make sure its going where you expect.
+                // return x2Meters.zeroOdometryOnStart();
+                // return getRightBox.resetOdometryOnStart();
 
-    /**
-     * Use this to pass the autonomous command to the main {@link Robot} class.
-     *
-     * @return the command to run in autonomous
-     */
-    public Command getAutonomousCommand() {
-        // * Simple tests
-        // Use these to verify odometry, gains, and encoder stuff. For the x2Meters, lay
-        // out a measuring tape to verify its moving 2 meters, and for the getRightBox,
-        // set it up on the field and make sure its going where you expect.
-        // return x2Meters.zeroOdometryOnStart();
-        // return getRightBox.resetOdometryOnStart();
+                // Command ejectAndArmDown = new SequentialCommandGroup(autoArmDown,
+                // ejectCrate);
+                // SequentialCommandGroup getBoxOne = new SequentialCommandGroup(new
+                // ParallelCommandGroup(getRightBox.resetOdometryOnStart(), intakeCrate), armUp,
+                // dropOffRightBox, ejectAndArmDown);
+                // SequentialCommandGroup getBoxTwo = new SequentialCommandGroup(new
+                // ParallelCommandGroup(getRightBox2, intakeCrate), armUp, dropOffRightBox2,
+                // ejectAndArmDown);
+                // return crawlBg;
+                // * 1 Box
+                return new SequentialCommandGroup(
+                                new ParallelCommandGroup(getRightBox.setOdometryToFirstPoseOnStart(), intakeCrate),
+                                autoArmUp, dropOffRightBox, autoArmDown, ejectCrate, backupAndCenter);
 
-        // Command ejectAndArmDown = new SequentialCommandGroup(autoArmDown,
-        // ejectCrate);
-        // SequentialCommandGroup getBoxOne = new SequentialCommandGroup(new
-        // ParallelCommandGroup(getRightBox.resetOdometryOnStart(), intakeCrate), armUp,
-        // dropOffRightBox, ejectAndArmDown);
-        // SequentialCommandGroup getBoxTwo = new SequentialCommandGroup(new
-        // ParallelCommandGroup(getRightBox2, intakeCrate), armUp, dropOffRightBox2,
-        // ejectAndArmDown);
-        // return crawlBg;
-        // * 1 Box
-        return new SequentialCommandGroup(
-                new ParallelCommandGroup(getRightBox.setOdometryToFirstPoseOnStart(), intakeCrate),
-                autoArmUp, dropOffRightBox, autoArmDown, ejectCrate, backupAndCenter);
+                // * 2 Boxes
+                // return new SequentialCommandGroup(new SequentialCommandGroup(new
+                // ParallelCommandGroup(getRightBox.resetOdometryOnStart(), intakeCrate), armUp,
+                // dropOffRightBox, new SequentialCommandGroup(autoArmDown, ejectCrate)), new
+                // SequentialCommandGroup(new ParallelCommandGroup(getRightBox2, intakeCrate),
+                // armUp, dropOffRightBox2, new SequentialCommandGroup(autoArmDown,
+                // ejectCrate)), backupAndCenter2);
 
-        // * 2 Boxes
-        // return new SequentialCommandGroup(new SequentialCommandGroup(new
-        // ParallelCommandGroup(getRightBox.resetOdometryOnStart(), intakeCrate), armUp,
-        // dropOffRightBox, new SequentialCommandGroup(autoArmDown, ejectCrate)), new
-        // SequentialCommandGroup(new ParallelCommandGroup(getRightBox2, intakeCrate),
-        // armUp, dropOffRightBox2, new SequentialCommandGroup(autoArmDown,
-        // ejectCrate)), backupAndCenter2);
-
-        // * Dry runs
-        // return new SequentialCommandGroup(getRightBox.resetOdometryOnStart(),
-        // dropOffRightBox, backupAndCenter);
-        // return new SequentialCommandGroup(getRightBox.resetOdometryOnStart(),
-        // dropOffRightBox, getRightBox2, dropOffRightBox2, backupAndCenter2);
-    }
+                // * Dry runs
+                // return new SequentialCommandGroup(getRightBox.resetOdometryOnStart(),
+                // dropOffRightBox, backupAndCenter);
+                // return new SequentialCommandGroup(getRightBox.resetOdometryOnStart(),
+                // dropOffRightBox, getRightBox2, dropOffRightBox2, backupAndCenter2);
+        }
 }
